0286c3454b163cebaa62c3e500684870be1c5b0e
Jake Howard <git@theorangeone.net>
2020-05-24 11:40:00 +0100

    Add arg parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::path::PathBuf;
+use structopt::StructOpt;
+
+#[derive(StructOpt, Debug)]
+#[structopt()]
+struct Opt {
+    #[structopt(short, long)]
+    debug: bool,
+
+    #[structopt(name="source", parse(from_os_str))]
+    sources: Vec<PathBuf>,
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let opts = Opt::from_args();
+    for elem in opts.sources.into_iter() {
+        println!("{:?}", elem)
+    }
 }
