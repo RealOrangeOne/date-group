e5da8b593172949959b7395fdf9fdc97537e0079
Jake Howard <git@theorangeone.net>
2020-05-24 14:18:35 +0100

    Parse exif datetile

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ use glob::glob;
 use std::fs::File;
 use std::io::BufReader;
 use exif::{In, Reader, Tag};
+use chrono::NaiveDateTime;
+use dtparse;
 
 
 #[derive(StructOpt, Debug)]
@@ -16,21 +18,37 @@ struct Opt {
     sources: Vec<PathBuf>,
 }
 
-fn process_file(file_path: PathBuf) -> String {
-    println!("{:?}", file_path);
-    let file = File::open(&file_path).expect("File not found");
+fn parse_datetime(date_time: String) -> Option<NaiveDateTime> {
+    let parsed = dtparse::parse(&date_time);
+    return match parsed {
+        Ok((dt, _)) => Some(dt),
+        Err(_) => None
+    };
+}
+
+fn read_exif_date(file_path: &PathBuf) -> Option<NaiveDateTime> {
+    let file = File::open(file_path).expect("File not found");
     let exif = Reader::new().read_from_container(&mut BufReader::new(&file)).expect("Failed to read");
     let val = exif.get_field(Tag::DateTime, In::PRIMARY).expect("Failed to get datetime");
-    return val.display_value().to_string();
+    return parse_datetime(val.display_value().to_string());
 }
+
+fn process_file(file_path: PathBuf) {
+    let exif_date = read_exif_date(&file_path);
+    println!("{:?}: {:?}", file_path, exif_date);
+}
+
 
 fn process_directory(path: PathBuf) {
     let mut path = path.clone();
     path.push("*");
     for f in glob(path.to_str().expect("Failed to convert path")).expect("Failed to glob") {
         match f {
-            Ok(f) => process_file(f),
-            Err(_) => String::new()
+            Ok(f) => {
+                println!("Processing {:?}", f);
+                process_file(f);
+            }
+            Err(_) => ()
         };
     }
 }
