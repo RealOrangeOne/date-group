5816fc9327da3c5e1c40fb3b70236ad914a55bd7
Jake Howard <git@theorangeone.net>
2020-05-24 12:11:15 +0100

    Enumerate directories

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::path::PathBuf;
 use structopt::StructOpt;
+use glob::glob;
 
 #[derive(StructOpt, Debug)]
 #[structopt()]
@@ -11,10 +12,25 @@ struct Opt {
     sources: Vec<PathBuf>,
 }
 
+fn process_file(file: PathBuf) {
+    println!("{:?}", file);
+}
+
+fn process_directory(path: PathBuf) {
+    let mut path = path.clone();
+    path.push("*");
+    for f in glob(path.to_str().expect("Failed to convert path")).expect("Failed to glob") {
+        match f {
+            Ok(f) => process_file(f),
+            Err(_) => ()
+        }
+    }
+}
+
 
 fn main() {
     let opts = Opt::from_args();
-    for elem in opts.sources.into_iter() {
-        println!("{:?}", elem)
+    for path in opts.sources.into_iter() {
+        process_directory(path);
     }
 }
