b8695cacf0d0879b3e1f68c262fbc13573da5d60
Jake Howard <git@theorangeone.net>
2020-05-24 13:22:48 +0100

    Process files

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 use std::path::PathBuf;
 use structopt::StructOpt;
 use glob::glob;
+use std::fs::File;
+use std::io::BufReader;
+use exif::{In, Reader, Tag};
+
 
 #[derive(StructOpt, Debug)]
 #[structopt()]
@@ -12,8 +16,12 @@ struct Opt {
     sources: Vec<PathBuf>,
 }
 
-fn process_file(file: PathBuf) {
-    println!("{:?}", file);
+fn process_file(file_path: PathBuf) -> String {
+    println!("{:?}", file_path);
+    let file = File::open(&file_path).expect("File not found");
+    let exif = Reader::new().read_from_container(&mut BufReader::new(&file)).expect("Failed to read");
+    let val = exif.get_field(Tag::DateTime, In::PRIMARY).expect("Failed to get datetime");
+    return val.display_value().to_string();
 }
 
 fn process_directory(path: PathBuf) {
@@ -22,8 +30,8 @@ fn process_directory(path: PathBuf) {
     for f in glob(path.to_str().expect("Failed to convert path")).expect("Failed to glob") {
         match f {
             Ok(f) => process_file(f),
-            Err(_) => ()
-        }
+            Err(_) => String::new()
+        };
     }
 }
 
