f015dd7189ea35518154d9f3ac202a470c4c730a
Jake Howard <git@theorangeone.net>
2020-05-29 17:40:50 +0100

    Catch and ignore panics inside parsing library
    This is a *huge* hack, but there's lots of `.unwrap`s in there, so I don't have much choice!

--- src/utils.rs
+++ src/utils.rs
@@ -1,18 +1,30 @@
 use chrono::NaiveDateTime;
 use dtparse::Parser;
 use std::collections::HashMap;
+use std::panic;
 
 pub fn parse_datetime(date_time: String) -> Option<NaiveDateTime> {
-    return match Parser::default().parse(
-        &date_time,
-        None,
-        None,
-        true,
-        false,
-        None,
-        true,
-        &HashMap::new(),
-    ) {
+    // HACK: Date parsing is hard!
+    let parse_match = panic::catch_unwind(|| {
+        return Parser::default().parse(
+            &date_time,
+            None,
+            None,
+            true,
+            false,
+            None,
+            true,
+            &HashMap::new(),
+        );
+    });
+    if parse_match.is_err() {
+        eprintln!(
+            "This panic was caused by attempting to parse '{}'.\n",
+            date_time
+        );
+        return None;
+    }
+    return match parse_match.ok()? {
         Ok((dt, _, _)) => Some(dt),
         Err(_) => None,
     };
