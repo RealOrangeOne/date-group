9bd2e0512a358199ab70db8dac0c8fd6edf27c62
Jake Howard <git@theorangeone.net>
2020-06-12 22:07:13 +0100

    Remove panic hack for parsing
    Now that https://github.com/bspeice/dtparse/issues/21 is fixed

--- src/utils.rs
+++ src/utils.rs
@@ -1,12 +1,11 @@
 use chrono::NaiveDateTime;
 use dtparse::Parser;
 use std::collections::HashMap;
-use std::panic;
 
 pub fn parse_datetime(date_time: String) -> Option<NaiveDateTime> {
     // HACK: Date parsing is hard!
-    let parse_match = panic::catch_unwind(|| {
-        return Parser::default().parse(
+    return Parser::default()
+        .parse(
             &date_time,
             None,
             None,
@@ -15,17 +14,7 @@ pub fn parse_datetime(date_time: String) -> Option<NaiveDateTime> {
             None,
             true,
             &HashMap::new(),
-        );
-    });
-    if parse_match.is_err() {
-        eprintln!(
-            "This panic was caused by attempting to parse '{}'.\n\n\n",
-            date_time
-        );
-        return None;
-    }
-    return match parse_match.ok()? {
-        Ok((dt, _, _)) => Some(dt),
-        Err(_) => None,
-    };
+        )
+        .map(|(dt, _, _)| dt)
+        .ok();
 }
