9e8f998b728adf50a271310f85926966c37c5d22
Jake Howard <git@theorangeone.net>
2020-06-27 12:31:08 +0100

    Add tests for more utils

--- src/utils.rs
+++ src/utils.rs
@@ -36,11 +36,36 @@ fn validate_datetime(date_time: NaiveDateTime) -> Option<NaiveDateTime> {
 mod tests {
 
     use super::*;
+    use chrono::NaiveDate;
+
+    fn make_datetime(year: i32, month: u32, day: u32, hour: u32, minute: u32, second: u32) -> NaiveDateTime {
+        return NaiveDate::from_ymd(year, month, day).and_hms(hour, minute, second);
+    }
+
     #[test]
     fn test_is_between() {
         assert!(is_between(2, 1, 3));
         assert!(is_between(200, 100, 300));
         assert!(is_between(100, 100, 100));
         assert!(is_between(-10, -100, 100));
+    }
+
+    #[test]
+    fn test_parse_datetime() {
+        assert!(parse_datetime("foobar".to_string()).is_none());
+        assert_eq!(
+            parse_datetime("2020-01-01".to_string()).unwrap(),
+            make_datetime(2020, 1, 1, 0, 0, 0)
+        );
+        assert_eq!(
+            parse_datetime("image 2020-01-01 03:04:05".to_string()).unwrap(),
+            make_datetime(2020, 1, 1, 3, 4, 5)
+        );
+    }
+
+    #[test]
+    fn test_validate_datetime() {
+        assert!(validate_datetime(make_datetime(2020, 1, 1, 3, 4, 5)).is_some());
+        assert!(validate_datetime(make_datetime(1820, 1, 1, 3, 4, 5)).is_none());
     }
 }
